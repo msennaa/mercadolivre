bc41e75a7cc02c9b99846cc40c3572e0bf264ad6
Seu nome <senamatheus.job@gmail.com>
2022-03-23 16:29:22 -0300

    Funçao para lidar com a API de busca de produtos

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { getProductsFromCategoryAndQuery } from '../services/API';
 
 export default function Main() {
+  const [productList, setProductList] = useState([]);
+
+  const teste = async () => {
+    setProductList(await getProductsFromCategoryAndQuery('computador'));
+  };
+
+  useEffect(() => {
+    teste();
+  }, []);
+
+  console.log(productList);
+
   return (
     <div>Main</div>
   );
